017498e3ea1b2c8c813f8c3c7a5aa1cb0f72274f
Mayank Mishra <mayank.mishra2@ibm.com>
2025-05-20 01:33:59 -0400

    exclude file
    Signed-off-by: Mayank Mishra <mayank.mishra2@ibm.com>

--- copyright.py
+++ copyright.py
@@ -8,7 +8,7 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument("--repo", type=str, required=True)
-parser.add_argument("--exclude", type=str, nargs="+", default=[], required=False)
+parser.add_argument("--exclude", type=str, required=False)
 parser.add_argument("--header", type=str, required=True)
 args = parser.parse_args()
 
@@ -26,7 +26,13 @@ _PYTHON_LIKE_EXTENSIONS = [
 ]
 _HTML_LIKE_EXTENSIONS = [".html", ".md"]
 
-_BANNED_DIRECTORIES = [os.path.realpath(i) for i in [".git"] + args.exclude]
+_BANNED = [".git"]
+if args.exclude:
+    exclude = open(args.exclude, "r").readlines()
+    exclude = [i.strip() for i in exclude]
+    _BANNED.extend(exclude)
+
+_BANNED = [os.path.realpath(i) for i in _BANNED]
 
 _CPP_HEADER = (
     f"// **************************************************\n"
@@ -59,10 +65,10 @@ def _check_and_add_copyright_header(file: str, header: str) -> None:
     open(file, "w").writelines([code])
 
 
-def _is_banned_directory(path: str) -> bool:
+def _is_banned(path: str) -> bool:
     assert not path.endswith("/")
 
-    for banned_directory in _BANNED_DIRECTORIES:
+    for banned_directory in _BANNED:
         if path.startswith(banned_directory):
             return True
 
@@ -72,11 +78,14 @@ def _is_banned_directory(path: str) -> bool:
 directory = os.path.realpath(args.repo)
 
 for root, dirs, files in os.walk(directory):
-    if _is_banned_directory(root):
+    if _is_banned(root):
         continue
 
     for file in files:
         file = os.path.join(root, file)
+
+        if _is_banned(file):
+            continue
 
         if any([file.endswith(i) for i in _CPP_LIKE_EXTENSIONS]):
             _check_and_add_copyright_header(file, _CPP_HEADER)
